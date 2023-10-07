4a9b637fb160da3439165b361edcd66204978ad8
Chayanon Pissanuwattanasak <chayanoon747@gmail.com>
2023-10-07 18:17:13 +0700

    page sign in and connect firebase

---
+++ firebase/app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8wFVPluXKr_ifTYcJzQ6MhiNTtLx333I",
+    authDomain: "fragrance-d2e8d.firebaseapp.com",
+    projectId: "fragrance-d2e8d",
+    storageBucket: "fragrance-d2e8d.appspot.com",
+    messagingSenderId: "112317818428",
+    appId: "1:112317818428:web:b5c3802add549fb6cfc539",
+    measurementId: "G-J88V463RCG"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
