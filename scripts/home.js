017c01b163d6ce619bc0205ef0ebebd48ac07ff2
Phumsith Insakul <Phumsit.i@ku.th>
2023-11-03 19:00:29 +0700

    update home (route to collection page)

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,6 @@
+const urlParams = new URLSearchParams(window.location.search);
+const userUID = urlParams.get('uid');
 
+document.querySelector('.banner-text').addEventListener('click', function() {
+    window.location.href = `./collection.html?uid=${userUID}`;
+});
